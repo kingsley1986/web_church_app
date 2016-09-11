efc03fa368231739084389be867f7e1100c46eb3
kingsley <chukwumakingsley1@gmail.com>
2016-09-11 20:41:07 +0200

    add it

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -4,7 +4,7 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
                     path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename'
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/aws.yml",
-                    :bucket => 'shipmetrics'
+                    :bucket => 'kingbucket'
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 100.megabytes
