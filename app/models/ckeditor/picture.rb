8fc92a06681b53184cce20aa5c11a6551eb515f1
kingsley <chukwumakingsley1@gmail.com>
2016-09-12 09:56:17 +0200

    heroku conf setting

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -3,6 +3,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                     styles: { content: '800>', thumb: '118x100#' }
+                    :storage => :s3,
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 2.megabytes
