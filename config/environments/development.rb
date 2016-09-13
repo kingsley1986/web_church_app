9b1b7211c983877f6dab5d8065c50c478cd00ea8
kingsley <chukwumakingsley1@gmail.com>
2016-09-13 18:17:37 +0200

    seting keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,11 +46,11 @@ Rails.application.configure do
   CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider               => 'AWS',
-        :aws_access_key_id      => ENV['S3_KEY'],
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
         :aws_secret_access_key  => ENV['S3_SECRECT'],
         :endpoint               => "https://s3.amazonaws.com",
         :region                 => ENV['S3-REGION']
     }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = ENV['S3_BUCKET']
   end
 end
