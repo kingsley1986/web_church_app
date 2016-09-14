fadd44a59edaa626f8e680cd38796c13a3bed480
kingsley <chukwumakingsley1@gmail.com>
2016-09-14 18:40:18 +0200

    pics for comment

--- app/uploaders/picture_image_uploader.rb
+++ app/uploaders/picture_image_uploader.rb
@@ -27,7 +27,7 @@ class PictureImageUploader < CarrierWave::Uploader::Base
  end
 
  version :thumb, :from_version => :medium do
-   process resize_to_fit: [100, 100]
+   process resize_to_fit: [200, 200]
  end
 
  version :square do
