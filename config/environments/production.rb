3fbec51276a348c368b189b032f878eb095d2e68
kingsley <chukwumakingsley1@gmail.com>
2016-10-16 00:03:38 +0200

    change background image and top bar to transparent

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  config.assets.precompile += ['homepages.scss']
+
 
   CarrierWave.configure do |config|
     config.fog_credentials = {
