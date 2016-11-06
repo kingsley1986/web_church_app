7e9ec3156d2746ba918688257fc75d58d8443778
kingsley <chukwumakingsley1@gmail.com>
2016-11-07 00:53:59 +0200

    corrected variable inresize image

--- app/uploaders/picture_image_uploader.rb
+++ app/uploaders/picture_image_uploader.rb
@@ -30,7 +30,7 @@ class PictureImageUploader < CarrierWave::Uploader::Base
    process :resize_to_fill => [250, 250]
  end
 
- version :thumb do
+ version :thumb, :from_version => :medium do
    process resize_to_fill: [200, 200]
  end
 
