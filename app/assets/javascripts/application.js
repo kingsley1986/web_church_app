48a8aa78fd44be265d15e9321e423630f5bbc78d
Kingsley <chukwumakingsley1@gmail.com>
2017-03-25 18:15:07 +0200

    rearranged things for javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery.turbolinks
+//= require jquery
 //= require jquery_ujs
-//= require lightbox
 //= require bootstrap-sprockets
+//= require lightbox
 //= require_tree .
 
 /**
