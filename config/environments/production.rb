8ff4b21d817603c5de74fd3bc4eefd36c7ba014f
Kingsley <chukwumakingsley1@gmail.com>
2017-05-23 00:03:30 +0200

    removed unwanted images and files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.serve_static_assets = true
+
+# Compress JavaScripts and CSS
+config.assets.compress = false
+
+# Don't fallback to assets pipeline if a precompiled asset is missed
+config.assets.compile = false
+
+# Generate digests for assets URLs
+config.assets.digest = true
+
   config.serve_static_files = true
   config.action_dispatch.x_sendfile_header = nil
   # Prepend all log lines with the following tags.
