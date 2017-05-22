1639842525b89e6c816a0da0f10120885072c341
Kingsley <chukwumakingsley1@gmail.com>
2017-05-23 00:49:35 +0200

    checking javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,9 +59,6 @@ jssor_1_slider_init = function() {
 
     var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
 
-    /*responsive code begin
-
-    /*remove responsive code if you don't want the slider scales while window resizing*/
     function ScaleSlider() {
         var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
         if (refSize) {
@@ -76,5 +73,4 @@ jssor_1_slider_init = function() {
     $Jssor$.$AddEvent(window, "load", ScaleSlider);
     $Jssor$.$AddEvent(window, "resize", ScaleSlider);
     $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*responsive code end*/
 };
